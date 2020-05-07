94579ecbeaf4e8bf4b9a2222ac2d0a00a22d5c3e
cydry3 <ysaita09@gmail.com>
2020-05-08 05:59:56 +0900

    Make it possible to make a convertion table for some objects by pid.

--- gen_asm_table.py
+++ gen_asm_table.py
@@ -2,20 +2,72 @@
 
 # output a table that convert address to assembly line.
 
-# prerequsite: only accepts a disassembled file by `objdump -D [target executable]`
-# usage: gen_asm_table.py [disassembled filename]
+# prerequsite: could disassemble a object file with `objdump -D [target executable]`
+# usage: gen_asm_table.py [pid]
 
 import sys
 import re
+import os
+import subprocess
 
-src_file = sys.argv[1]
+def conv_table(pid):
+    desc_file = 'table_addr2asm'
+    tmp_tables = {}
+    obj_addr_pairs= read_child_proc_file(pid)
+    for obj_path, base_addr in obj_addr_pairs.items():
+        dumped_file = obj_dump(pid, obj_path)
+        tmp_tables[dumped_file] = base_addr
+    write_tables(desc_file, tmp_tables)
+    cleanup(tmp_tables)
 
-desc_filename= 'table_addr2asm' 
-desc_file= open(desc_filename, 'w')
+def read_child_proc_file(pid):
+    obj_addr_pairs= {}
+    child_proc_file = "/proc/%s/maps" % pid
+    with open(child_proc_file, "r") as proc_file:
+        re_pat = re.compile('^(\w+)-.+:\w+\ \w+\ +(/.+)')
+        for line in proc_file:
+            m = re_pat.match(line)
+            if m:
+                if not (m.group(2) in obj_addr_pairs):
+                    obj_addr_pairs[m.group(2)] = m.group(1)
+    return obj_addr_pairs
 
-disassembled_file = open(src_file, 'r')
-for line in disassembled_file:
-    m = re.match('^(\ +)(.*)', line)
+def obj_dump(pid, obj_path):
+    tmp_table_name = "tmp_table_addr2asm_" + pid + obj_path.replace('/', '_').replace('.', '_')
+    with open(tmp_table_name, "w") as conv_file:
+        p = subprocess.Popen(["/usr/bin/objdump", "-D", obj_path], stdout=conv_file)
+        p.wait()
+    return tmp_table_name
 
-    if m:
-        desc_file.write("%s\n" % m.group(2))
+def write_tables(desc_filename, tmp_tables):
+    with open(desc_filename, 'a') as desc_file:
+        for tmp_filename, base_addr in tmp_tables.items():
+            write_table(desc_file, tmp_filename, base_addr)
+
+def write_table(desc_file, disassembled_file, base_addr):
+    base_addr = int(base_addr, 16)
+    with open(disassembled_file, 'r') as disasm_file:
+        for line in disasm_file:
+            m = re.match('^\ +(\w+):(.*)', line)
+            if m:
+                offset_addr = int(m.group(1), 16)
+                offset_addr += base_addr
+                desc_file.write("%x:%s\n" % (offset_addr, m.group(2)))
+
+def cleanup(tmp_tables):
+    for fname in tmp_tables.keys():
+        os.remove(fname)
+
+
+def script_main():
+    arglen = len(sys.argv)
+    if arglen != 2:
+        print("usage: gen_asm_table.py [pid]")
+        sys.exit(1)
+    else:
+        pid = sys.argv[1]
+        conv_table(pid)
+
+# Execute as command
+if __name__ == "__main__":
+    script_main()
